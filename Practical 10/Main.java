4acd831bad2f42536d0f9a6302b53b3f32138c01
Sahil Zalte <zaltesahil94@gmail.com>
2025-03-02 22:22:55 +0530

    Practical 10

---
+++ Practical 10/Main.java
@@ -0,0 +1,8 @@
+import MyPackage.MyClass; // Importing the user-defined package
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass(); // Creating an object of MyClass
+        obj.displayMessage(); // Calling the method
+    }
+}
---
+++ Practical 10/MyPackage/MyClass.java
@@ -0,0 +1,7 @@
+package MyPackage; // Declaring the package
+
+public class MyClass {
+    public void displayMessage() {
+        System.out.println("Hello from MyPackage!");
+    }
+}
